d7b447ccfa63478b25a226d22e58505294d9899b
Marek <marek.marchlewicz@gmail.com>
2024-02-18 12:27:45 +0800

    Added SetRootBone and simplified SetBoneTransforms

--- Scripts/SpriteSkinHelpers.cs
+++ Scripts/SpriteSkinHelpers.cs
@@ -12,7 +12,8 @@ public static class AnimationHelpers
     static readonly Type k_SpriteSkinType = k_RuntimeAssembly.GetType($"{k_AnimationRuntimeNamespace}.SpriteSkin");
     static readonly PropertyInfo k_SpriteSkinGetAutoRebind = k_SpriteSkinType.GetProperty("autoRebind", BindingFlags.Instance | BindingFlags.NonPublic);
     static readonly PropertyInfo k_SpriteSkinIsValid = k_SpriteSkinType.GetProperty("isValid", BindingFlags.Instance | BindingFlags.NonPublic);
-    static readonly MethodInfo k_SpriteSkinBoneTransforms = k_SpriteSkinType.GetMethod("set_boneTransforms", BindingFlags.Instance | BindingFlags.NonPublic);
+    static readonly PropertyInfo k_SpriteSkinRootBone = k_SpriteSkinType.GetProperty("rootBone", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+    static readonly PropertyInfo k_SpriteSkinBoneTransforms = k_SpriteSkinType.GetProperty("boneTransforms", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
 
     /// <summary>
     /// Gets the value of Auto Rebind property.
@@ -29,6 +30,18 @@ public static class AnimationHelpers
     public static void SetAutoRebind(this SpriteSkin spriteSkin, bool autoRebind) => k_SpriteSkinGetAutoRebind.SetValue(spriteSkin, autoRebind);
 
     /// <summary>
+    /// Sets the Transform Component that represents the root bone for deformation.
+    /// </summary>
+    /// <param name="spriteSkin">Sprite Skin component.</param>
+    /// <param name="rootBone">A Transform Component of the root bone.</param>
+    /// <returns>True if successful.</returns>
+    public static bool SetRootBone(this SpriteSkin spriteSkin, Transform rootBone)
+    {
+        k_SpriteSkinRootBone.SetValue(spriteSkin, rootBone);
+        return (bool)k_SpriteSkinIsValid.GetValue(spriteSkin);
+    }
+
+    /// <summary>
     /// Sets the Transform Components that are used for deformation.
     /// </summary>
     /// <param name="spriteSkin">Sprite Skin component.</param>
@@ -36,7 +49,7 @@ public static class AnimationHelpers
     /// <returns>True if successful.</returns>
     public static bool SetBoneTransforms(this SpriteSkin spriteSkin, Transform[] boneTransformsArray)
     {
-        k_SpriteSkinBoneTransforms.Invoke(spriteSkin, new object[] { boneTransformsArray });
+        k_SpriteSkinBoneTransforms.SetValue(spriteSkin, boneTransformsArray);
         return (bool)k_SpriteSkinIsValid.GetValue(spriteSkin);
     }
 }
